99d1adfd9b5cd849119d3edcde8b5d587f3fd397
Pankaj Kumar <97273195+gtpankajjha@users.noreply.github.com>
2024-10-08 08:23:15 +0000

    DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId= 1234343;
+let accountEmail= "pankaj@gmail.com";
+var accountPassword= "12345";
+accountCity="Jaipur";
+let accountState;
+
+
+// accountId=2; //Assignment to constant variable.
+//console.log(accountId);
+
+accountEmail="rahul@gmail.com"
+accountPassword="123221"
+accountCity="Banglore"
+
+/*
+Prefer not to use var beacuse of issues in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code in New verion of javacript
+
+//alert(3+4) // we are using nodejs, not browser
+
+let name="Pankaj";
+let age=28;
+let isLoggedIn=true;
+
+//DataType in Javascript
+/*
+number=2
+bigint
+string=""
+boolean= true/fasle
+null =stand alone value/ representation to empty value //it is typeof is object
+undefined
+symbol=> unquie
+object
+*/
