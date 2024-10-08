a26e6c705a0530c81449d9fb60d6252e86a112d5
Pankaj Kumar <97273195+gtpankajjha@users.noreply.github.com>
2024-10-08 09:14:18 +0000

    converion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score="34";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+
+//"34" => 34
+//"34abc" => NaN
+//null =>0
+//true=> 1 false=>0
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=> true; 0=>false
+// ""=> false
+//"Pankaj"=> true
+
+let someNumber=33
+let stringNumber=S tring(someNumber)
+console.log(stringNumber)
